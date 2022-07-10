15c5f1986e95851f0e2884959b16c2ba710be7f3
usergitvv <valery.voronkov@yandex.ru>
2022-07-10 14:11:53 +0500

    Add arguments and format-option in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
